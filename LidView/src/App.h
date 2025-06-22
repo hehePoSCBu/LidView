542553be815425bf9e503fcf5f77adfb8861c7de
hehePoSCBu <hehelili6z@outlook.com>
2025-06-22 20:42:45 +0800

    添加settings.h

--- LidView/src/App.h
+++ LidView/src/App.h
@@ -7,3 +7,5 @@
 
 #include<glad/glad.h>
 #include<GLFW/glfw3.h>
+
+#include"settings.h"
--- LidView/src/main.cpp
+++ LidView/src/main.cpp
@@ -6,31 +6,10 @@
 #include <vector>
 #include <memory>
 
-std::vector<float> vertices = {
-	//     ---- 位置 ----       ---- 颜色 ----     - 纹理坐标 -
-		 0.5f,  0.5f, 0.0f,   1.0f, 0.0f, 0.0f,   1.0f, 1.0f,   // 右上
-		 0.5f, -0.5f, 0.0f,   0.0f, 1.0f, 0.0f,   1.0f, 0.0f,   // 右下
-		-0.5f, -0.5f, 0.0f,   0.0f, 0.0f, 1.0f,   0.0f, 0.0f,   // 左下
-		-0.5f,  0.5f, 0.0f,   1.0f, 1.0f, 0.0f,   0.0f, 1.0f    // 左上
-};
-
-std::vector<float> texCoord = {
-	1.0f, 1.0f, // 右上
-	1.0f, 0.0f, // 右下
-	0.0f, 0.0f, // 左下
-	0.0f, 1.0f  // 左上
-};
-
-// Add default indices for a quad (two triangles)
-std::vector<unsigned int> indices = {
-	0, 1, 2,
-	0, 2, 3
-};
-
 int main()
 {
 	Application* app = new Application();
-	auto triangle = std::make_unique<Object>(
+	auto test = std::make_unique<Object>(
 		vertices,
 		texCoord, // Pass texCoords
 		indices,  // Pass indices
@@ -38,7 +17,7 @@ int main()
 		"E:/dev/LidView/LidView/src/Shader/Objects/TriangleShader.frag",
 		"E:/dev/LidView/assets/container.jpg" // Texture path
 	);
-	app->add_object(std::move(triangle));
+	app->add_object(std::move(test));
 
 	app->run();
 	return 0;
